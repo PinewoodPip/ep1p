e4d39cbf67f97691becdc703dadbbea31dbde4b5
Kamil <lekamilthemage@gmail.com>
2026-04-09 08:47:47 +0200

    Fix DeadPartyXP not considering the setting

--- DOSEE-Extender/DeadPartyXP.cpp
+++ DOSEE-Extender/DeadPartyXP.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "DeadPartyXP.h"
+#include "Epip/EpipSettings.h"
 #include <Extender/ScriptExtender.h>
 
 using namespace dse;
@@ -7,13 +8,15 @@ using namespace epip;
 
 void DeadPartyXP::Setup()
 {
-	bool applied = true;
-	// Remove check for tactician/honour mode
-	applied = applied && gExtender->GetLibraryManager().ApplyCodePatch("esv::Character::AddExperience patch");
-	applied = applied && gExtender->GetLibraryManager().ApplyCodePatch("ecl::Character::AddExperience patch");
-
-	if (!applied)
+	if (gSettings->DeadPartyMemberXP)
 	{
-		LOG("Not all XP patches succeeded"); // TODO show error for user, undo patches
+		bool applied = true;
+		// Remove check for tactician/honour mode
+		applied = applied && gExtender->GetLibraryManager().ApplyCodePatch("esv::Character::AddExperience patch");
+		applied = applied && gExtender->GetLibraryManager().ApplyCodePatch("ecl::Character::AddExperience patch");
+		if (!applied)
+		{
+			LOG("Not all XP patches succeeded"); // TODO show error for user, undo patches
+		}
 	}
 }
